6bed0a5fd42e1c66a60c97f37a196fdc9c464e16
Ross Hemsley <rlhemsley@gmail.com>
2011-04-13 18:30:09 +0100

    Rewriting l-interval stuff

--- src/esa.c
+++ src/esa.c
@@ -198,12 +198,67 @@ int str_gth(const char *a, const char *b, int n, int *i)
 }
 
 /******************************************************************************/
+// Return the child interval that starts with the given character
+// at position l.
+// This takes at most O(|\Sigma|) time.
+
+int getInterval(int *i, int *j, int l, char c, const ESA *esa)
+{
+
+   int u=0;
+
+   if (*i==0 && *j == esa->n-1)
+      u = LI_NEXT_CHILD(u,esa);
+   else
+      u = LI_FIRST_CHILD(*i,*j, esa);
+
+
+
+   if (esa->t[esa->SA[*i]+l] == c)
+   {    
+      // If this is the end of the interval.
+      if (u!=0) *j = u-1;
+
+      return 1;
+   } 
+
+
+   do
+   {
+      //printf("u: %d\n", u);
+      //printf("Comparing to: %c\n", esa->t[esa->SA[u]+l]);
+      if (esa->t[esa->SA[u]+l] == c)
+      {
+         *i = u;
+
+         
+         int v = LI_NEXT_CHILD(u,esa);
+         
+         // If this is the end of the interval.
+         if (v!=0) *j = v-1;
+         
+         return 1;
+      }
+
+      u = LI_NEXT_CHILD(u, esa);
+      //printf("Next child: %d\n", u);
+   
+   } while (u != 0);
+   
+   //printf("No such interval\n");
+   
+   // The interval was not found.
+   return 0;
+}
+
+/******************************************************************************/
 // Find the first location of a the longest substring in O(n\log m) 
 // [ O(n + \log m)  expected time ].
 // This algorithm comes from Gusfield.
 
 // l0 is the start position, r0 is the end position.
 // l is the length found.
+
 int findLongestSubstring(        const char*     p,                                
                                        int       m,   
                                        int*      l,                                                              
@@ -333,7 +388,168 @@ int findSubstringPosition(       const char*     p,
 }
 /******************************************************************************/
 
+int findLongestSubstring2(       const char*     p,                                
+                                       int       m,   
+                                       int*      l,                                                              
+                                       int       l0, 
+                                       int       r0, 
+                                 const ESA*      esa                           )
+{
+
+   int c=0;   
+   int i=0;
+   int j=esa->n-1;
+
+   int fail = 0;
+  
+      
+   
+   while (getInterval(&i, &j, c, p[c], esa) && c<m)
+   {
+      
+    //  printf("Now in interval %d, %d\n", i, j);
+      
+      
+      if (i!=j)
+      {
+         int lcp = LI_GET_LCP(i,j,esa);
+         if (lcp > m)
+            lcp = m;
+      
+         fail = str_gth( esa->t + esa->SA[i], p, lcp, &c);         
+         c    = lcp;
+         
+      } 
+      else
+      {
+         fail = str_gth( esa->t + esa->SA[i], p, m, &c);                      
+         break;  
+      }
+   }
+      
+   *l = c;   
+      
+   if (!fail)
+      return esa->SA[i];   
+
+   return -1;
+
+}                                 
+
+
+
+
 /*
+
+
+
+
+
+
+
+   // v is the start of the next l-index.
+   int v = LI_FIRST_CHILD(i, j, esa);
+      
+   // We use this to force a stop on the next iteration.
+   int STOP = 0;
+
+   // Now, keep traversing the tree until we run out of possibilities. 
+   while (1)
+   {
+      
+      // Singleton.
+      // We have reached a leaf node.
+      if (i == j)
+      {  
+        // printf("Singleton interval\n");
+         while  (text[l-1] != '\0' && (pattern + esa->SA[u])[l] == text[l])
+            l++;
+         break;
+      }
+                 
+      // Find the l-value of this l-interval.
+      int lcp = LI_GET_LCP(i,j,esa);
+      
+      // Set this to zero if the matching failed.
+      int match = 1;
+      
+      // This will walk us down branches which are multiple symbols long.
+      for (; l < lcp ; l++)
+      {
+         if ((pattern + esa->SA[u])[l] != text[l])
+         {
+            match = 0;
+            break;
+         }
+      }
+      
+      if (match == 0)
+         break;
+         
+      // Does the start of this l-interval match?
+      if ( (pattern + esa->SA[u])[l] == text[l] )       
+      {
+         // Increment the 'depth' in the text.
+         ++l;      
+         
+         // If we are in the last interval and find a match, then 
+         // we want to continue looking down the tree and so we do not stop yet.
+         STOP = 0;
+
+         // Move down the tree.
+         i = u;
+         j = v-1;m
+         
+         // find the index of the first child interval in the new l-interval.
+         v = LI_FIRST_CHILD(i,j,esa);
+        
+         continue;
+      }  
+      
+      // Move accross the tree to the next child interval.
+      u = v;
+      v = esa->accross[v];
+   
+      // If we have reached the end of this l-interval,
+      // then v will be 0. However, there could still be a match
+      // here, so we use STOP to indicate that the next round should
+      // be the last.
+      // Obviously, if we find a match, STOP should be reset.            
+      if (STOP)
+         break;
+      
+      // We have reached the end of this l-interval.
+      // Setting v=j+1 will mean that the end of the interval will be set to 
+      // j later on.
+      // We want to stop on the next iteration of there are no matches.
+      if (v == 0)
+      {
+         v = j+1;m
+         STOP = 1;
+      }       
+   }
+   
+   // put our 'results' into this p-block.
+   P->l = l;
+   P->j = esa->SA[i];
+
+   // Return the longest match we found.
+   return l;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Find the maximum possible extension of any suffix and this part of the text 
 // and store it in P.
 static inline int extendInterval(const int*      LOOKUP, 
@@ -470,9 +686,9 @@ void _randomStrings(char *text, char *pattern ,int n, int m)
 
    for (i=0; i<n;i++)
       // random letter from a..b
-      text[i] =  (char)(rand() % 2 + 97);
+      text[i] =  (char)(rand() % 4 + 97);
    for (i=0;i<m;i++)
-      pattern[i] = (char)(rand() % 2 + 97);
+      pattern[i] = (char)(rand() % 4 + 97);
 }
 
 /******************************************************************************/
@@ -485,28 +701,46 @@ int test_ESA()
    srand( time(NULL) );
          
    int n       = 1e3;
-   int m       = 2e2;
+   int m       = 200;
    int repeats = 1e4;
          
    // The text and pattern strings.
-   char t[n+1];
-   char p[m+1];
-   p[m] = '\0';
-   t[n] = '\0';
+   char t[n];
+   char p[m];
+   p[m-1] = '\0';
+   t[n-1] = '\0';
+   
       
    ESA esa;
    
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
    for (int i=0; i<repeats; i++)
    {
-      _randomStrings(t,p,n,m);
+      _randomStrings(t,p,n-1,m-1);
     
       // Throw away p and set it to a substring of t.   
       memcpy(p,t,sizeof(char)*m);
             
-      constructESA(t,n+1, &esa, NO_RMQ);
+      constructESA(t,n, &esa, NO_RMQ);
       int l=0;
     
       findLongestSubstring(p, m, &l,0,n, &esa);
+
+      assert(l == m);
+      if (l != m) return 1;    
+    
+      findLongestSubstring2(p, m, &l,0,n, &esa);
       
       // A full match should be found.
       assert(l == m);
